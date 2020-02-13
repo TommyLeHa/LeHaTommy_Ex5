23286f945397f814a16dd05dd68e419cfeddeb24
Tommy Le Ha <leha.tommy.pro@gmail.com>
2020-02-13 18:36:28 -0500

    Modification des fichiers de travail de l'exercice 05

--- mjs/anim_entete.mjs
+++ mjs/anim_entete.mjs
@@ -1,13 +1,16 @@
-export class anim_entete {
-    constructor(elmEntete){
-        this.elmEntete = elmEntete;
-    }
+export class anim_entete{
+constructor(elmEntete){
+    this.elmEntete = elmEntete;
+}
 
-    anim(){
-        console.log('animation');
-        console.log(this.elmEntete.tagName)
-        let elmTitre = this.elmEntete.children[0];
-        elmTitre.classList.add('animEntete')
-    }
+anim(){
+    console.log('//////////anim///////////')
+    console.log(this.elmEntete.tagName)
+    let elmTitre = this.elmEntete[0];
+    elmTitre.classList.add('animEntete');
     
+
+
+}
+
 }
